e1c09ddc0c94b2f277c599c169167c3b61e949b5
Silver Light <ywang16@email.wm.edu>
2021-12-22 22:38:06 -0500

    Initial Commit

---
+++ src/components/EditableRow.js
@@ -0,0 +1,90 @@
+import React from "react";
+
+const EditableRow = ({
+  editFormData,
+  handleEditFormChange,
+  handleCancelClick,
+}) => {
+  return (
+    <tr>
+      <td>
+        <input
+          type="text"
+          required="required"
+          placeholder="Enter a name..."
+          name="fullName"
+          value={editFormData.fullName}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+        <input
+          type="text"
+          required="required"
+          placeholder="Enter an address..."
+          name="address"
+          value={editFormData.address}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+        <input
+          type="text"
+          required="required"
+          placeholder="Enter a phone number..."
+          name="phoneNumber"
+          value={editFormData.phoneNumber}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+        <input
+          type="email"
+          required="required"
+          placeholder="Enter an email..."
+          name="email"
+          value={editFormData.email}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+        <input
+          type="text"
+          required="required"
+          placeholder="Enter a department..."
+          name="email"
+          value={editFormData.department}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+        <input
+          type="text"
+          required="required"
+          placeholder="Enter a title..."
+          name="title"
+          value={editFormData.title}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+        <input
+          type="text"
+          required="required"
+          placeholder="Enter a location..."
+          name="location"
+          value={editFormData.location}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+        <button type="submit">Save</button>
+        <button type="button" onClick={handleCancelClick}>
+          Cancel
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default EditableRow;
